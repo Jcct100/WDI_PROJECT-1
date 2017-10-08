35cb1f965adcfb7503c3451ac2a7bd372b3899ed
Jcct100 <jamestang_@outlook.com>
2017-10-08 21:24:27 +0100

    copy2

--- js/app.js
+++ js/app.js
@@ -1,54 +1,20 @@
 
-//pesudo code:
-//1)one click or type event,
-//2)time interval to start the loop
-//3)loop through the boxes, reverse?
-//4)each loop show a different colour and hide it.
-//5)and loop back.?
 
-//challenge will be the timing of my functions being called? callbacks? setup scope.
-
-//make a click event for box6. //that click must be able to access the loop
-
-//click change color to black.
-//read about time interval and show/hide
+//pesudo code
+//1)you have 60 seconds to get as many hits as possible once its up display score.
+//2)each hit is one point (score display)
+//3)each hit will alert a fact about elephants
+//3b)each hit will make a sound.
+//4)click play to start?
+//5)click reset to reset game?
+//6)the game gets more difficult as the time the image appear will speed up.
+//7)music will play in the background
 
 $(() => {
 
 
-  // const $box = $('#box6');
-  //
-  //
-  // $box.on('click', function() {
-  //  console.log('clicked');
-  // });
-
-const $box = $('.box6');
-$box.on('click', jump);
-
-function jump() {
-    $box.animate({'left': '+=100px' },'fast' );
-}
-
-
-// function jump() {
-//     // const $wrapper = $('div');
-//     console.log(jQuery.isPlainObject($box));
-//     //  $wrapper.show();
-//
-// }
-
-  // const $wrapper = $('li');
-  // for (var i = 5; i >= 0; i--)
-
-
-// if ($wrapper[i] === 3) {
-
-
-
-
-  // const $wrapper = $('.wrapper');
-  // $wrapper.hide( 'slow', 'swing');
+// const $box = $('body');
+// $box.on('click', jump);
 
 
 
