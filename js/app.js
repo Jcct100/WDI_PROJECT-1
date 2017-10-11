379006bcdae661d80d7b510e611075afb4419260
Jcct100 <jamestang_@outlook.com>
2017-10-11 20:05:19 +0100

    Fixing some issues

--- js/app.js
+++ js/app.js
@@ -3,19 +3,40 @@
 //landing page, audio, images, emoji adn style tomorrow or today if finished.
 
 //different levels.
-//1)add a timer.
-//2)when timer is over 20 then add flying objects.
+//1)add a timer. reset timer is not working.
+//2)when timer is under 20 then add flying objects.
 //3)and when timer is over 30 seconds then add bigger divs.
 
 $(() => {
 
+  let counter = 0;
+  let lives = 3;
+  let jumping;
+
   // let activeJump = false;
   const $click = $('body');
   const $PlayerBox = $('.box');
-  let counter = 0;
-  let lives = 3;
   const $reset = $('button');
   const $time =  $('.timer');
+
+
+  //reset the game
+  $reset.on('click', reset);
+
+  function reset() {
+    lives = 3;
+    time = 30;
+    $time.html(time);
+    $PlayerBox.show();
+    $reset.hide();
+    boxInterval = setInterval(createobtacles, 5000);
+    setInterval(timer);
+  }
+
+  // when time is 20 add level 2
+  // if (time === 20) {
+  //   setInterval(createobtacles2, 5000);
+  // }
 
   //set timer
   let time = 30;
@@ -26,31 +47,34 @@ $(() => {
   }, 1000);
 
   function checkValue() {
-    if (time <= 0) {
+    if (time < 30 ) {
+      console.log('hi');
+    }  else if (time === 0) {
+      console.log('ok');
       clearInterval(timer);
     }
   }
 
-  //reset the game
-  $reset.on('click', reset);
-  function reset() {
-    lives = 3;
-    $PlayerBox.show();
-    $reset.hide();
-    boxInterval = setInterval(createobtacles, 5000);
-  }
-
-  $click.on('click', jump);
-
   //make PlayerBox jump
 
-  function jump() {
+  $click.on('keydown', jump);
+
+  function jump(e) {
+    if (e.keyCode !== 32 || jumping) return false;
+    jumping = true;
+
     // activeJump = true
-    $PlayerBox.animate({ 'bottom': '500'  }, 800, drop);
-    console.log(counter);
+    $PlayerBox.animate({
+      'bottom': '200'
+    }, 'slow', 'swing', drop);
   }
+
   function drop() {
-    $PlayerBox.animate({ 'bottom': '0' });
+    $PlayerBox.animate({
+      'bottom': '0'
+    }, 1000, () => {
+      jumping = false;
+    });
     // activeJump = false
   }
 
@@ -68,20 +92,21 @@ $(() => {
   //create new obtacles
 
 
-  let boxInterval = setInterval(createobtacles, 5000);
-  createobtacles();
+  let boxInterval = setInterval(createobtacles, Math.floor(Math.random() * 3000) + 1000);
+  // createobtacles();
 
   function createobtacles() {
     const $newObtacle = $('<div class="obtacle"></div>');
+    $newObtacle.css('right', '-300px');
     $('body').append($newObtacle);
     animateBox($newObtacle);
   }
 
   function animateBox($newObtacle) {
     $newObtacle.animate({
-      'right': `${$('body').width()}`
+      'right': `${$('body').width()*1.5}`
     }, {
-      duration: 6000,
+      duration: 3000,
       step: function() {
         if (collisions($PlayerBox, $newObtacle)) {
           console.log('game over');
@@ -92,6 +117,7 @@ $(() => {
             $PlayerBox.hide();
             $reset.show();
             clearInterval(boxInterval);
+            clearInterval(timer);
           }
         }
       },
@@ -105,6 +131,45 @@ $(() => {
 
     });
 
+
+  //   let boxInterval2 = setInterval(createobtacles2, 5000);
+  //   // createobtacles();
+  //
+  //   function createobtacles2() {
+  //     const $newObtacle2 = $('<div class="obtacle2"></div>');
+  //     $('body').append($newObtacle2);
+  //     animateBox($newObtacle2);
+  //   }
+  //
+  //   function animateBox($newObtacle2) {
+  //     $newObtacle2.animate({
+  //       'right': `${$('body').width()}`
+  //     }, {
+  //       duration: 6000,
+  //       step: function() {
+  //         if (collisions($PlayerBox, $newObtacle2)) {
+  //           console.log('game over');
+  //           // alert('game over')
+  //           $newObtacle2.remove();
+  //           lives -=1;
+  //           if (lives === 0) {
+  //             $PlayerBox.hide();
+  //             $reset.show();
+  //             clearInterval(boxInterval);
+  //             clearInterval(timer);
+  //           }
+  //         }
+  //       },
+  //       complete: function() {
+  //         $newObtacle2.remove();
+  //         counter +=1;
+  //         if (counter % 3 === 0 ) {
+  //           PoPupMessage();
+  //         }
+  //       }
+  //
+  //     });
+  //
     function collisions($div1, $div2) {
       var x1 = $div1.offset().left;
       var y1 = $div1.offset().top;
@@ -121,14 +186,9 @@ $(() => {
       if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
       return true;
     }
-
+  //
   }
 
-
-
-
-
 //when click to reset game
-
 
 });
